d81223d0d936c3d3dfe21ab89c8e69528d2509bc
Irfan Rafif <mirfanrafif17@gmail.com>
2022-05-25 08:50:46 +0700

    remove client id from env

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import Button from "../../components/Button";
 import { Heading1, Text } from "../../components/Typography";
 import { color } from "../../components/Color";
-import { randomString } from "../../utils/RandomHelper";
 
 export default function SignIn() {
   function signInWithGithub() {
-    const state = randomString(30);
-
-    window.location.href = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_ID}&redirect_uri=${process.env.REACT_APP_GITHUB_REDIRECT_URI}&scope=${process.env.REACT_APP_GITHUB_SCOPE}&state=${state}&allow_signup=true`;
+    window.location.href = `${process.env.REACT_APP_API_URL}/api/authorize/github`;
   }
 
   return (
